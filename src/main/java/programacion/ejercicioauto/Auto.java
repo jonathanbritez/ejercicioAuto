b9bead433ad77a860b824a1807fc71df259cb353
jonathanbritez <jonathanmix2016@hotmail.com>
2021-03-25 22:12:38 -0300

    Initial commit

---
+++ src/main/java/programacion/ejercicioauto/Auto.java
@@ -0,0 +1,26 @@
+
+package programacion.ejercicioauto;
+/**
+ * Crear la clase Auto con los atributos marca, modelo y kilometraje,
+ * elegir el tipo de dato adecuado para ellos. 
+ * Además se debe crear el método estadoAuto() que depende el kilometraje muestre un mensaje:
+
+0 km Esta nuevo
+<10000km Poco usado
+<100000km Usado
+>100000km Bastante usado
+
+Crear un objeto de esta clase y comprobar el funcionamiento
+*/
+//creo la clase auto
+public class Auto {
+    
+    public static void main(String arg[]){
+    
+       
+    
+        estadoAuto Funcion = new estadoAuto();
+        Funcion.velocidad();
+    }
+    
+}
